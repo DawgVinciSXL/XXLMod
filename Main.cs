0dfe5817b40e4af4c5044b250c41b00b097ecb95
Marcel Klopsch <Jakihz@gmail.com>
2022-12-27 04:29:55 +0100

    Update

--- Main.cs
+++ Main.cs
@@ -1,12 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HarmonyLib;
+using RapidGUI;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
 
 namespace XXLMod
 {
     public class Main
     {
+        public static string Version = "";
+
+        public static Harmony HarmonyInstance;
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static Settings Settings;
+        public static UnityModManager.ModEntry modEntry;
+
+        public static bool enabled;
+
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Settings = UnityModManager.ModSettings.Load<Settings>(modEntry);
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = new Action<UnityModManager.ModEntry>(OnSaveGUI);
+            modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(OnToggle);
+            Main.modEntry = modEntry;
+            return true;
+        }
+
+        private static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.Box("<b>Background Color</b>", GUILayout.Height(21f));
+            Settings.BGColor.r = RGUI.SliderFloat(Settings.BGColor.r, 0f, 1f, 0f, "Red");
+            Settings.BGColor.g = RGUI.SliderFloat(Settings.BGColor.g, 0f, 1f, 0f, "Green");
+            Settings.BGColor.b = RGUI.SliderFloat(Settings.BGColor.b, 0f, 1f, 0f, "Blue");
+            GUILayout.Box("<b>Hotkeys</b>", GUILayout.Height(21f));
+            Settings.Draw(modEntry);
+        }
+
+        private static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            Settings.Save(modEntry);
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            if (value == enabled)
+            {
+                return true;
+            }
+
+            enabled = value;
+
+            if (enabled)
+            {
+                HarmonyInstance = new Harmony(modEntry.Info.Id);
+                HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+
+            }
+            else
+            {
+                HarmonyInstance.UnpatchAll(HarmonyInstance.Id);
+                ResetToDefault();
+            }
+            return true;
+        }
+
+        private static void ResetToDefault()
+        {
+        }
     }
 }
---
+++ Settings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace XXLMod
+{
+    [Serializable]
+    public class Settings : UnityModManager.ModSettings, IDrawable
+    {
+        [Draw(DrawType.KeyBinding)] public KeyBinding MainMenu = new KeyBinding { keyCode = KeyCode.F7 };
+        [Draw(DrawType.KeyBinding)] public KeyBinding StanceCustomizer = new KeyBinding { keyCode = KeyCode.U };
+        [Draw(DrawType.KeyBinding)] public KeyBinding SteezeCustomizer = new KeyBinding { keyCode = KeyCode.I };
+        [Draw(DrawType.KeyBinding)] public KeyBinding GrabCustomizer = new KeyBinding { keyCode = KeyCode.O };
+        [Draw(DrawType.KeyBinding)] public KeyBinding Presets = new KeyBinding { keyCode = KeyCode.P };
+        [Draw(DrawType.KeyBinding)] public KeyBinding PinManager = new KeyBinding { keyCode = KeyCode.Space };
+
+        public float NotificationTimer = 2f;
+        public bool Experimental = false;
+        public bool Debugging = false;
+        public Color BGColor = new Color(0f, 0f, 0f);
+
+        public void OnChange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save<Settings>(this, modEntry);
+        }
+    }
+}
